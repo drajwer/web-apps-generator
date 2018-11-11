281004b9ec078000a415b5e35434d206bdb8aa04
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2018-11-11 06:52:16 +0100

    Added dependency injection

--- WebAppsGenerator.Console/Program.cs
+++ WebAppsGenerator.Console/Program.cs
@@ -1,8 +1,11 @@
-﻿using Antlr4.Runtime;
+﻿using System;
+using Antlr4.Runtime;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using WebAppsGenerator.Core.Files;
 using WebAppsGenerator.Core.Files.Providers;
 using WebAppsGenerator.Core.Grammar;
+using WebAppsGenerator.Core.Interfaces;
 using WebAppsGenerator.Core.Parsing.Annotations;
 using WebAppsGenerator.Core.Parsing.Types;
 
@@ -11,9 +14,12 @@ namespace WebAppsGenerator.Console
     internal class Program
     {
         public static IConfiguration Configuration { get; set; }
+        public static IServiceProvider ServiceProvider { get; set; }
 
         private static void Main(string[] args)
         {
+            ConfigureServices();
+
             var concatFileService = new ConcatFileService(new FlatDirectoryFilesProvider("./../../../TestDir", "txt"));
             var inputStream = new AntlrInputStream(concatFileService.ConcatFile);
             var lexer = new SneakLexer(inputStream);
@@ -23,12 +29,12 @@ namespace WebAppsGenerator.Console
             var parser = new SneakParser(commonTokenStream);
             var fileContext = parser.file();
 
-            var visitor = new SneakParserCustomVisitor(new BasicTypeParser(), new BasicAnnotationParamParser());
+            var visitor = (SneakParserCustomVisitor) ServiceProvider.GetService<ISneakParserVisitor<object>>();// new SneakParserCustomVisitor(new BasicTypeParser(), new BasicAnnotationParamParser());
 
             visitor.Visit(fileContext);
             
             // pass visitor's results to generator
-            var generator = new Generator.Generator.Generator();
+            var generator = ServiceProvider.GetService<IGenerator>();
             generator.ProcessVisitorResults(visitor.Entities.Values);
 
             foreach (var token in commonTokenStream.GetTokens())
@@ -36,6 +42,19 @@ namespace WebAppsGenerator.Console
             System.Console.WriteLine(concatFileService.ConcatFile);
 
             System.Console.ReadKey();
+        }
+
+        private static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddOptions();
+            services.AddScoped<ISneakParserVisitor<object>, SneakParserCustomVisitor>();
+            services.AddTransient<ITypeParser, BasicTypeParser>();
+            services.AddTransient<IAnnotationParamParser, BasicAnnotationParamParser>();
+            services.AddScoped<IGenerator, Generator.Generator.Generator>();
+
+            ServiceProvider = services.BuildServiceProvider();
         }
     }
 }
