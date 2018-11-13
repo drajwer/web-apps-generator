7cc40f21fecffd5e2c685d1f4b94b4f753093d62
Kacper Drejer <kacper.drejer@gmail.com>
2018-11-13 23:26:54 +0100

    add command line service

---
+++ WebAppsGenerator.Core/Services/CommandLineService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebAppsGenerator.Core.Services
+{
+    public class CommandLineService
+    {
+        private const string Cmd = "cmd.exe";
+        private const string Bash = "/bin/bash";
+        private static PlatformID CurrentPlatformId => Environment.OSVersion.Platform;
+
+        private Action<string> _outputAction;
+        private Action<string> _errorAction;
+
+
+        public CommandLineService()
+        {
+            _outputAction = Console.WriteLine;
+            _errorAction = Console.WriteLine;
+        }
+        public CommandLineService(Action<string> outputAction, Action<string> errorAction)
+        {
+            _outputAction = outputAction;
+            _errorAction = errorAction;
+        }
+
+        public void RunCommand(string command)
+        {
+            var shell = GetShell();
+            var arguments = GetArguments(command);
+
+            var processInfo = new ProcessStartInfo(shell, arguments)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (sender, e) => _outputAction(e.Data);
+            process.ErrorDataReceived += (sender, e) => _errorAction(e.Data);
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+        }
+
+        private static string GetArguments(string command)
+        {
+            if (CurrentPlatformId == PlatformID.Unix || CurrentPlatformId == PlatformID.MacOSX)
+                return $"-c \"{command}\"";
+            return $"/c \"{command}\"";
+        }
+
+        private static string GetShell()
+        {
+            if (CurrentPlatformId == PlatformID.Unix || CurrentPlatformId == PlatformID.MacOSX)
+                return Bash;
+            return Cmd;
+        }
+    }
+}
