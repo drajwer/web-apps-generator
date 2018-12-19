d4ce11fa8af65dcc27da61ae17914868936156de
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2018-12-19 09:16:59 +0100

    Post-merge fixes

--- WebAppsGenerator.Generating.Abstract/Models/Templating/EntityDrop.cs
+++ WebAppsGenerator.Generating.Abstract/Models/Templating/EntityDrop.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using DotLiquid;
 using WebAppsGenerator.Core.Models;
 using WebAppsGenerator.Generating.Abstract.Services;
@@ -24,6 +22,22 @@ namespace WebAppsGenerator.Generating.Abstract.Models.Templating
             PluralName = PluralizationHelper.Pluralize(Name);
             Fields = entity.Fields.Select(f => new FieldDrop(f)).ToList();
             IdField = Fields.FirstOrDefault(f => f.Name == "Id");
+
+            CamelCasePluralName = char.ToLowerInvariant(PluralName[0]) + PluralName.Substring(1);
+            CamelCaseName = char.ToLowerInvariant(Name[0]) + Name.Substring(1);
+            SnakeUppercasePluralName = ToSnakeUppercase(PluralName);
+            SnakeUppercaseName = ToSnakeUppercase(Name);
+        }
+
+        public string CamelCasePluralName { get; }
+        public string CamelCaseName { get; }
+        public string SnakeUppercasePluralName { get; }
+        public string SnakeUppercaseName { get; }
+
+
+        private static string ToSnakeUppercase(string input)
+        {
+            return string.Concat(input.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString().ToUpper()));
         }
     }
 }
--- WebAppsGenerator.Generating.WebUi/Models/Templating/SingleEntityDrop.cs
+++ WebAppsGenerator.Generating.WebUi/Models/Templating/SingleEntityDrop.cs
@@ -1,5 +1,6 @@
 ﻿using WebAppsGenerator.Core.Models;
 using WebAppsGenerator.Generating.Abstract.Interfaces;
+using WebAppsGenerator.Generating.Abstract.Models.Templating;
 using WebAppsGenerator.Generating.WebUi.Services;
 
 namespace WebAppsGenerator.Generating.WebUi.Models.Templating
@@ -9,15 +10,15 @@ namespace WebAppsGenerator.Generating.WebUi.Models.Templating
         public SingleEntityDrop(IGeneratorConfiguration generatorConfiguration, SolutionPathService solutionPathService, Entity entity)
             : base(solutionPathService, generatorConfiguration)
         {
-            Entity = new EnhancedEntityDrop(entity);
+            Entity = new EntityDrop(entity);
         }
 
-        public SingleEntityDrop(IGeneratorConfiguration generatorConfiguration, SolutionPathService pathService, EnhancedEntityDrop entity)
+        public SingleEntityDrop(IGeneratorConfiguration generatorConfiguration, SolutionPathService pathService, EntityDrop entity)
             : base(pathService, generatorConfiguration)
         {
             Entity = entity;
         }
 
-        public EnhancedEntityDrop Entity { get; }
+        public EntityDrop Entity { get; }
     }
 }
