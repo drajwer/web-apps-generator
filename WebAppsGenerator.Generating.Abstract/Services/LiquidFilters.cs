5d5ae9117a02eecbebe975bc80ec9c05a2d37261
Kacper Drejer <kacper.drejer@gmail.com>
2018-12-22 00:52:37 +0100

    add custom liquid filters

--- WebAppsGenerator.Generating.Abstract/Services/LiquidFilters.cs
+++ WebAppsGenerator.Generating.Abstract/Services/LiquidFilters.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WebAppsGenerator.Generating.Abstract.Services
 {
     public class LiquidFilters
     {
+        public static string DeCapitalize(string text)
+        {
+            if (text?.Length == 0)
+                return text;
+            if (text.Length == 1)
+                return char.ToLower(text[0]).ToString();
+
+            return $"{char.ToLower(text[0])}{text.Substring(1, text.Length - 1)}";
+        }
         public static string SplitCamelCase(string text)
         {
             string result = "";
