613403d8a2b9d44a9e126ac97e1abe0224244733
Kacper Drejer <kacper.drejer@gmail.com>
2018-12-22 00:38:19 +0100

    add swagger and add fixed port for web api

---
+++ WebAppsGenerator.Generating.Abstract/Services/LiquidFilters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAppsGenerator.Generating.Abstract.Services
+{
+    public class LiquidFilters
+    {
+        public static string SplitCamelCase(string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (char.IsUpper(c) && i > 0)
+                    result += ' ';
+                result += c;
+            }
+
+            return result;
+        }
+    }
+}
