936d4a7953ab1fa4a82e0d7519f3aa67a5d99749
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2018-12-29 15:35:51 +0100

    Added navigation betweent tables and forms

--- WebAppsGenerator.Generating.AspNetCore/Services/SolutionGenerator.cs
+++ WebAppsGenerator.Generating.AspNetCore/Services/SolutionGenerator.cs
@@ -12,7 +12,7 @@ namespace WebAppsGenerator.Generating.AspNetCore.Services
     /// </summary>
     public class SolutionGenerator : IGenerator
     {
-        private const bool IsEnabled = true;
+        private const bool IsEnabled = false;
 
         private readonly ICommandLineService _commandLineService;
         private readonly IGenerator _webApiProjectGenerator;
@@ -38,12 +38,12 @@ namespace WebAppsGenerator.Generating.AspNetCore.Services
             if (entities == null)
                 throw new ArgumentNullException();
 
-            //CreateSolutionWithProjects();
+            CreateSolutionWithProjects();
 
-            //AddNuGetPackages($"{_pathService.CoreDirPath}\\{_pathService.CoreProjectName}.csproj", _generatorConfiguration.CoreProjectPackages);
+            AddNuGetPackages($"{_pathService.CoreDirPath}\\{_pathService.CoreProjectName}.csproj", _generatorConfiguration.CoreProjectPackages);
 
-            //_webApiProjectGenerator.Generate(entities);
-            //_coreProjectGenerator.Generate(entities);
+            _webApiProjectGenerator.Generate(entities);
+            _coreProjectGenerator.Generate(entities);
         }
 
         private void CreateSolutionWithProjects()
