b883258d965b2353a1719dfdf348af4e70cf0101
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-01-06 14:38:35 +0100

    Added ValuesController deletion

--- WebAppsGenerator.Generating.AspNetCore/Services/WebApiProjectGenerator.cs
+++ WebAppsGenerator.Generating.AspNetCore/Services/WebApiProjectGenerator.cs
@@ -1,4 +1,7 @@
-﻿using WebAppsGenerator.Generating.Abstract.Interfaces;
+﻿using System.Collections.Generic;
+using System.IO;
+using WebAppsGenerator.Core.Models;
+using WebAppsGenerator.Generating.Abstract.Interfaces;
 using WebAppsGenerator.Generating.Abstract.Services;
 using WebAppsGenerator.Generating.AspNetCore.Interfaces;
 
@@ -6,10 +9,25 @@ namespace WebAppsGenerator.Generating.AspNetCore.Services
 {
     public class WebApiProjectGenerator : BaseGenerator
     {
+        private readonly SolutionPathService _solutionPathService;
+
         public WebApiProjectGenerator(IGeneratorConfiguration generatorConfiguration, IAspNetCoreFileService fileService,
-            IWebApiProjectTemplatingConfigProvider templatingConfigProvider, CSharpDropFactory dropFactory)
+            IWebApiProjectTemplatingConfigProvider templatingConfigProvider, CSharpDropFactory dropFactory, SolutionPathService solutionPathService)
             : base(generatorConfiguration, dropFactory, templatingConfigProvider, fileService)
         {
+            _solutionPathService = solutionPathService;
+        }
+
+        public override void Generate(IEnumerable<Entity> entities)
+        {
+            RemoveUnnecessaryFiles();
+
+            base.Generate(entities);
+        }
+
+        public void RemoveUnnecessaryFiles()
+        {
+            File.Delete(Path.Combine(_solutionPathService.WebApiDirPath, "Controllers/ValuesController.cs"));
         }
     }
 }
