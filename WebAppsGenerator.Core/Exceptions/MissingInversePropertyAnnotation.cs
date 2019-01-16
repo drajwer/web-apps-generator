bb67676f0c5c1a8d74596faae271acb5c2b1ea18
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-01-16 03:58:40 +0100

    Fixed relations

---
+++ WebAppsGenerator.Core/Exceptions/MissingInversePropertyAnnotation.cs
@@ -0,0 +1,12 @@
+﻿using WebAppsGenerator.Core.Models;
+
+namespace WebAppsGenerator.Core.Exceptions
+{
+    public class MissingInversePropertyAnnotation : ParsingException
+    {
+        public MissingInversePropertyAnnotation(string referencedEntityName, BaseModel model)
+            : base($"Cannot find corresponding field in class {referencedEntityName} to configure relationship", model)
+        {
+        }
+    }
+}
