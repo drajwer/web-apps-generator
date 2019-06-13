a946a4462e7bc9dac497be8cff54c8e0c625d879
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-06-13 23:15:41 +0200

    added base grammar tests

---
+++ WebAppsGenerator.Tests/Mocks/BaseGeneratorMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAppsGenerator.Generating.Abstract.Interfaces;
+using WebAppsGenerator.Generating.Abstract.Services;
+
+namespace WebAppsGenerator.Tests.Mocks
+{
+    public class BaseGeneratorMock : BaseGenerator
+    {
+        public new FileServiceMock FileService { get; }
+
+        public BaseGeneratorMock(IGeneratorConfiguration generatorConfiguration, IDropFactory dropFactory, ITemplatingConfigProvider configProvider, FileServiceMock fileService) : base(generatorConfiguration, dropFactory, configProvider, fileService)
+        {
+            FileService = fileService;
+        }
+    }
+}
